08d07a127f268058472856fec654abb6c073a1d5
RandomServant <89282619+RandomServant@users.noreply.github.com>
2023-01-27 17:20:38 +1000

    Updated main.cpp, added code for all modules

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    Testing *test = new Testing("../Tests/", 6);
+/*
+    Testing *test = new Testing("../Generated tests/", 20);
+    //test->StartGeneratingAndTestingVEB("f", true);
+    //test->StartGeneratingAndTestingSET("f", true);
+    //test->StartGeneratingAndTestingUNSET("f", true);
+*/
 
+    Testing *test = new Testing("../Tests/", 12);
     test->StartTesting();
 
     return 0;
